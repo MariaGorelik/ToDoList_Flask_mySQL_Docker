49598b508134a264bc813e7f97284bf2dd8d6466
Maria Gorelik <marya.gorelik@gmail.com>
2024-01-20 22:07:11 +0300

    Change sqlite for pymysql

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import pymysql
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dsfgj56547jgfkh90gh4ogot'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+
+try:
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='hytgbn',
+        database='todolist',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("successfully connected...")
+    print("#" * 20)
+
+except Exception as ex:
+    print("Connection refused...")
+    print(ex)
+
+
+def check_user(username, password):
+    with connection.cursor() as cursor:
+        query = f"SELECT * FROM users WHERE username = '{username}' AND userPassword = '{password}'"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        return result is not None
+
+
+@app.route('/main_page', methods=['GET', 'POST'])
+def main_page():
+    if not 'username' in session:
+        return redirect(url_for('login'))
+    with connection.cursor() as cursor:
+        user_query = f"SELECT * FROM users WHERE username = '{session['username']}'"
+        cursor.execute(user_query)
+        user = cursor.fetchone()
+        if request.method == 'GET':
+            notes_query = f"SELECT * FROM notes WHERE userID = {user['id']}"
+            cursor.execute(notes_query)
+            notes = cursor.fetchall()
+            return render_template('index.html', notes=notes)
+        # post
+        note = request.form.get('note')
+        if note:
+            insert_note = f"INSERT INTO notes (userID, content) VALUES ({user['id']}, '{note}')"
+            cursor.execute(insert_note)
+            connection.commit()
+            return redirect(url_for('main_page'))
+
+
+@app.route('/delete-note/<int:note_id>/')
+def delete_note(note_id):
+    if 'username' in session:
+        with connection.cursor() as cursor:
+            get_query = f"SELECT id FROM notes WHERE id = {note_id}"
+            cursor.execute(get_query)
+            result = cursor.fetchone()
+            if result is not None:
+                delete_query = f"DELETE FROM notes WHERE id = {note_id}"
+                cursor.execute(delete_query)
+                connection.commit()
+            return redirect(url_for('main_page'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_user(username, password):
+            session['username'] = username
+            return redirect(url_for('main_page'))
+        return render_template('login.html', error_message='Неверное имя пользователя или пароль')
+    return render_template('login.html')
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method != 'POST':
+        return render_template('sign_up.html')
+    username = request.form['username']
+    password = request.form['password']
+    with connection.cursor() as cursor:
+        user_query = f"SELECT * FROM users WHERE username = '{username}'"
+        cursor.execute(user_query)
+        isFound = cursor.fetchone()
+        if isFound is not None:
+            return render_template('sign_up.html', error_message='This username already exists')
+        add_user = f"INSERT INTO users (username, userPassword) VALUES ('{username}', '{password}')"
+        cursor.execute(add_user)
+        connection.commit()
+        session['username'] = username
+        return redirect(url_for('main_page'))
+
+
+@app.route('/logout')
+# @login_required
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Page not found", 404
+
+
+if __name__ == '__main__':
+    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+    app.run(debug=True, host='0.0.0.0')
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+import app
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+
+# if __name__ == '__main__':
+#     db.init_app(app)
+#     db.create_all()
